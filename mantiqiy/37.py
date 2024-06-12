568cf6c8efe4c3e061d48a8ecdf477489b65e8e1
amelikuziev <abrorbekmelikuziev@gmail.com>
2024-06-12 21:01:06 +0500

    36-38

--- mantiqiy/37.py
+++ mantiqiy/37.py
@@ -1,8 +1,11 @@
+
 x1 = int(input("x1 sonni kiriting: "))
 y1 = int(input("y1 sonni kiriting: "))
 
 x2 = int(input("x2 sonni kiriting: "))
 y2 = int(input("y2 sonni kiriting: "))
 
-result = (x1==x2 or y1==y2)
-print(result)
+resultx = (x1-1 <= x2) or (x2 <= x1 +1)
+resulty = (y1-1 <= y2) or (y2 <= x2+1 )
+r=resultx and resulty
+print(r)
